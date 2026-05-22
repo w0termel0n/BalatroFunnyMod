f1d010c099f8229d23127de26684f0dba00f61ee
muffinyug <cooper.oneal10@gmail.com>
2026-05-21 21:21:56 -0400

    Added Petah. Still needs work tho

--- jokers.lua
+++ jokers.lua
@@ -28,3 +28,35 @@ SMODS.Joker({
         end
     end,
 })
+SMODS.Joker({
+    key = "Peter",
+    config = {},
+    rarity = 2,
+    cost = 4,
+    unlocked = true,
+    discovered = true,
+    eternal_compat = true,
+    perishable_compat = true,
+
+    loc_txt = {
+        name = "Peter",
+        text = {
+            "Peter, the horse is here.",
+
+        }
+    },
+
+    calculate = function(self, card, context)
+        if context.last_hand then
+            return {
+                message = "hehehehehehehe",
+                colour = G.C.GREY
+            }
+        end
+        if context.joker_main then
+            return {
+                
+            }
+        end
+    end,
+})
